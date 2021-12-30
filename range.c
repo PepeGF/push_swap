305f08947aae00903e91d3b932390f26282e3490
PepeGF <gran.pepe@gmail.com>
2021-12-30 11:08:07 +0100

    parece que el segfault está en la funcion range, pendiente de debuggeo profundo

--- range.c
+++ range.c
@@ -6,7 +6,7 @@
 /*   By: josgarci <josgarci@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 20:00:39 by josgarci          #+#    #+#             */
-/*   Updated: 2021/12/30 00:11:18 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/12/30 10:55:08 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,25 @@ void	ft_range(t_list *lst)
 	int		order;
 	t_list	*aux;
 
+	(void)len;
 	len = ft_lstsize(lst);
 	order = 1;
 	while (order <= len)
 	{
 		aux = lst;
-		min_partial = ft_lstmin_partial(lst);
-		printf("zzz%dzzz\n",min_partial);
+	while (aux)
+	{
+		min_partial =  ft_lstmin_partial(lst);
 		if (aux->num == min_partial)
 		{
+			printf("**%d**\n", aux->num);
 			aux->range = order;
-			order++;
+			ft_print_list(lst);
 		}
+		else
 		aux = aux->next;
+	}
+	order++;
 	}
 }
 
@@ -43,16 +49,19 @@ static int	ft_lstmin_partial(t_list *lst)
 	t_list	*aux;
 
 	aux = lst;
-	while (aux->range != -1)
+	while (aux && aux->range != -1)
 	{
 		aux = aux->next;
 	}
 	min = aux->num;
+	if (aux != ft_lstlast(lst))
+	{
 	while (aux->next)
 	{
 		if (aux->next->num < min && aux->next->range == -1)
 			min = aux->next->num;
 		aux = aux->next;
+	}
 	}
 	return (min);
 }
